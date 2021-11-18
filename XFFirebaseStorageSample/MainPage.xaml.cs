4f605367bbe6e5ec7489640a7329bbec86181a97
Gerald Versluis <gerald@verslu.is>
2021-11-18 10:46:16 +0100

    Implement upload

--- XFFirebaseStorageSample/MainPage.xaml.cs
+++ XFFirebaseStorageSample/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Firebase.Storage;
 using Xamarin.Forms;
 
 namespace XFFirebaseStorageSample
@@ -13,6 +14,32 @@ namespace XFFirebaseStorageSample
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var photo = await Xamarin.Essentials.MediaPicker.PickPhotoAsync();
+
+            if (photo == null)
+                return;
+
+            var task = new FirebaseStorage("samplestorage-f4d9b.appspot.com",
+                new FirebaseStorageOptions
+                {
+                    ThrowOnCancel = true
+                })
+                .Child("DidYouSubscribe")
+                .Child("ToMyChannelYet")
+                .Child(photo.FileName)
+                .PutAsync(await photo.OpenReadAsync());
+
+            task.Progress.ProgressChanged += (s, args) =>
+            {
+                progressBar.Progress = args.Percentage;
+            };
+
+            var downloadlink = await task;
+            downloadLink.Text = downloadlink;
         }
     }
 }
